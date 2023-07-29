c51906dbf0d857083f84ec9605ace17fe3abad0c
KASHISH17RAMANI <102422940+KASHISHRAMANI@users.noreply.github.com>
2023-07-29 21:38:25 +0530

    first commit

---
+++ 8_bit_booth_multiplier.srcs/sim_1/new/booth_multiplier.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/12/2023 01:25:06 PM
+// Design Name: 
+// Module Name: booth_multiplier
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module booth_multiplier(
+
+    );
+endmodule
